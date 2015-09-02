953cc75d32483aece42f895de59aa76c6c440a13
Henry J. Wylde <public@hjwylde.com>
2015-09-02 19:10:25 +1200

    Added parsing to run arguments
    This now allowed you to pass in any type of argument, e.g., a list: `qux
    run foo.qux "[1, 2, 3]"'

--- src/Qux/Commands/Run.hs
+++ src/Qux/Commands/Run.hs
@@ -13,8 +13,12 @@ module Qux.Commands.Run where
 
 import Control.Monad.Except
 
+import qualified Data.Map as Map
+
+import Language.Qux.Annotated.Parser
 import Language.Qux.Annotated.Simplify
 import Language.Qux.Annotated.Syntax
+import Language.Qux.Annotated.TypeChecker
 import Language.Qux.Interpreter
 import Language.Qux.PrettyPrinter
 
@@ -41,8 +45,19 @@ handle options = do
         Right result    -> putStrLn result
 
 run :: Options -> Program a -> Except String String
-run options program = return $ render (pPrint result)
-    where
-        result = exec (sProgram program) (optEntry options) args
-        args = map (IntValue . read) (argsExtra options)
+run options program = do
+    args <- parseArgs $ argsExtra options
+
+    typeCheckArgs args
+
+    let result = exec (sProgram program) (optEntry options) args
+
+    return $ render (pPrint result)
+
+-- TODO (hjw): improve the error message (the source position is wrong)
+parseArgs :: [String] -> Except String [Value]
+parseArgs = mapM (withExcept show . parse value "command line")
+
+typeCheckArgs :: [Value] -> Except String ()
+typeCheckArgs = mapM_ (\value -> withExcept show $ runCheck (checkValue value) emptyContext Map.empty)
 
