04c0a5a2e1739e784d311d6c82cca9337f435ae7
Henry J. Wylde <public@hjwylde.com>
2016-09-15 13:58:57 +1200

    Empty Qux.Steps file for holding build steps

---
+++ src/Qux/Steps.hs
@@ -0,0 +1,13 @@
+{-|
+Module      : Qux.Steps
+Description : Build steps.
+
+Copyright   : (c) Henry J. Wylde, 2015
+License     : BSD3
+Maintainer  : public@hjwylde.com
+
+Build steps.
+-}
+
+module Qux.Steps (
+) where
